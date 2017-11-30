c53bb3ec63f5b30f330875f0fa22b27fce7d8b3d
Scott Woodend <woodendscott@gmail.com>
2017-11-30 14:09:15 -0500

    wire the click handlers and pass necessary state into the component from the store

--- src/components/InfoArea.js
+++ src/components/InfoArea.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { voteAction } from '../actions/voteAction';
 import '../styles/infoarea.css';
 
+
+@connect((state) => {
+    return {
+        votes: {
+            angular: state.votes.angular,
+            react: state.votes.react,
+            vue: state.votes.vue
+        },
+        currentPercent: {
+            angular: state.currentPercentages.angular,
+            react: state.currentPercentages.react,
+            vue: state.currentPercentages.vue
+        },
+        pastPercent: {
+            angular: state.pastPercentages.angular,
+            react: state.pastPercentages.react,
+            vue: state.pastPercentages.vue
+        }
+    }
+})
 export default class InfoArea extends Component {
+    constructor(props){
+        super(props);
+        this.votedAngular = this.votedAngular.bind(this);
+        this.votedReact = this.votedReact.bind(this);
+        this.votedVue = this.votedVue.bind(this);
+    }
+
+    votedAngular(){
+        console.log('voted angular');
+    }
+
+    votedReact(){
+        console.log('voted react');
+    }
+
+    votedVue(){
+        console.log('voted vue');
+    }
+
     render(){
         return (
         <div className="info-container">
-            <img style={{"marginTop":"25px"}} className="app-img" src={require("../../public/brainbulb.svg")}></img>
+            <img 
+                style={{"marginTop":"25px"}} 
+                className="app-img" 
+                src={require("../../public/brainbulb.svg")}>
+            </img>
             <h2 className="wide">What is your favourite front-end framework in 2017?</h2>
             <h4 style={{"marginTop": "0"}}>Click an image below to vote!</h4>
             <div className="row">
                 <div className="images-container">
-                    <img className="framework-logo" src={require("../../public/angular.png")}></img>
-                    <img className="framework-logo" src={require("../../public/react.png")}></img>
-                    <img className="framework-logo" src={require("../../public/vue.png")}></img>                        
+                    <img
+                        onClick={this.votedAngular} 
+                        className="framework-logo" 
+                        src={require("../../public/angular.png")}>
+                    </img>
+                    <img 
+                        onClick={this.votedReact}
+                        className="framework-logo" 
+                        src={require("../../public/react.png")}>
+                    </img>
+                    <img 
+                        onClick={this.votedVue}
+                        className="framework-logo"
+                        src={require("../../public/vue.png")}
+                    ></img>
                 </div>
             </div>
         </div>
