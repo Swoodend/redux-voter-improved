5c84bfaebd10293546b2f6fe0d3e9c168f5e6fd4
Scott Woodend <woodendscott@gmail.com>
2017-11-30 14:07:04 -0500

    remove test decorator

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import InfoArea from './InfoArea';
 import '../styles/app.css';
 
-function testDecorator(target){
-    target.working = true;
-}
 
-@testDecorator
 export default class App extends Component {
     render(){
         return (
