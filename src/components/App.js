2dbf443862b262f81252d4f72df7d80efd76ffbc
Scott Woodend <woodendscott@gmail.com>
2017-11-29 20:21:06 -0500

    App now renders InfoArea component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import InfoArea from './InfoArea';
 import '../styles/app.css';
-import testImg from '../../public/small.gif';
 
 function testDecorator(target){
     target.working = true;
@@ -10,12 +10,9 @@ function testDecorator(target){
 export default class App extends Component {
     render(){
         return (
-            <div>
-                <h1>Hello World</h1>
-                <img src={testImg}></img>
+            <div className="app">
+                <InfoArea />
             </div>
         );
     }
 }
-
-console.log('decorating working is', App.working);
