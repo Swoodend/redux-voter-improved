b8450552ef079bf9140bdd9c2b73ff919ae5b015
Scott Woodend <woodendscott@gmail.com>
2017-11-30 12:36:10 -0500

    import reducers and set up store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { votesReducer } from './reducers/votesReducer';
+import { currentPercentageReducer } from './reducers/currentPercentageReducer';
+import { pastPercentageReducer } from './reducers/pastPercentageReducer';
+import { combineReducers, createStore} from 'redux';
+
+const combinedReducers = combineReducers({
+    "votes": votesReducer,
+    "currentPercentages": currentPercentageReducer,
+    "pastPercentages": pastPercentageReducer
+});
+
+let store = createStore(combinedReducers);
+
 
 ReactDOM.render(
     <App />, 
