12a89680fe5f846db3a0fec609cff9b094dc5562
Scott Woodend <woodendscott@gmail.com>
2017-12-03 15:57:08 -0500

    When a user casts a vote the castVote action dispatches, this action dispatches 3 others to update the state of the store

--- src/actions/voteAction.js
+++ src/actions/voteAction.js
@@ -1,6 +1,26 @@
-export function voteAction(framework){
+const voteAction = (framework) => {
     return {
         type: "VOTE",
         payload: framework
     }
+}
+
+const updateCurrentPercentages = (voteState) => {
+    return {
+        type: "UPDATE_CURRENT_PERCENTAGES",
+        payload: voteState
+    }
+}
+
+const updatePastPercentages = (voteState) => {
+    return {
+        type: "UPDATE_PAST_PERCENTAGES",
+        payload: voteState
+    }
+}
+
+export const castVote = (framework) => (dispatch, getState) => {
+    dispatch(updatePastPercentages(getState().currentPercentages));
+    dispatch(voteAction(framework));
+    dispatch(updateCurrentPercentages(getState().votes));    
 }
