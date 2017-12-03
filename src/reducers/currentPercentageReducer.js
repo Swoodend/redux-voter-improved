7e41aa870c1d76ab5cbfae84f23102ff0d9b522c
Scott Woodend <woodendscott@gmail.com>
2017-12-03 15:58:04 -0500

    currentPercentagesReducer updates the current voting percentages

--- src/reducers/currentPercentageReducer.js
+++ src/reducers/currentPercentageReducer.js
@@ -5,5 +5,20 @@ const intitialPercentages = {
 }
 
 export function currentPercentageReducer(state=intitialPercentages, action){
-    return state;
+    switch (action.type){
+        case "UPDATE_CURRENT_PERCENTAGES":
+            let totalVotes = action.payload.angular + action.payload.react + action.payload.vue
+            let pctAngular = Math.round((action.payload.angular / totalVotes) * 100);
+            let pctReact = Math.round((action.payload.react / totalVotes) * 100);
+            let pctVue = Math.round((action.payload.vue / totalVotes) * 100);
+            
+            return Object.assign({}, state, {
+                angular: pctAngular,
+                react: pctReact,
+                vue: pctVue
+            });
+            
+        default:
+            return state
+    }
 }
