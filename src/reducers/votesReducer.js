34a2a483ee38e129ab672352e4d8b090c201e03b
Scott Woodend <woodendscott@gmail.com>
2017-12-03 15:59:07 -0500

    votesReducer updates store with current votes of all frameworks

--- src/reducers/votesReducer.js
+++ src/reducers/votesReducer.js
@@ -3,17 +3,17 @@ const initialVoteState = { angular: 0, react: 0, vue: 0 };
 export function votesReducer(state=initialVoteState, action){
     switch(action.type){
         case "VOTE":
-            if (action.framework === 'angular'){
+            if (action.payload === 'angular'){
                 return Object.assign({}, state, {
                     angular: state.angular + 1
                 });
-            } else if (action.framework === 'react'){
+            } else if (action.payload === 'react'){
                 return Object.assign({}, state, {
                     react: state.react + 1
                 });
             } else {
                 return Object.assign({}, state, {
-                    vue: state.view + 1
+                    vue: state.vue + 1
                 });
             }
         default:
