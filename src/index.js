a6cfcee5924301380d4e9750a5d188877e416bfe
Scott Woodend <woodendscott@gmail.com>
2017-12-03 22:39:21 -0500

    store creation/middleware functions now belong in the dedicated store file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { votesReducer } from './reducers/votesReducer';
-import { currentPercentageReducer } from './reducers/currentPercentageReducer';
-import { pastPercentageReducer } from './reducers/pastPercentageReducer';
-import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger'
-
-const combinedReducers = combineReducers({
-    "votes": votesReducer,
-    "currentPercentages": currentPercentageReducer,
-    "pastPercentages": pastPercentageReducer
-});
-
-const logger = createLogger();
-let store = createStore(combinedReducers, applyMiddleware(logger, thunk));
+import { store } from './store/store.js';
 
 ReactDOM.render(
     <Provider store={store}>
