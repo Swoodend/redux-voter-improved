782a37708ee0afe267c967dec7bcd2e0ed736346
Scott Woodend <woodendscott@gmail.com>
2017-12-03 22:09:27 -0500

    render the results area

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import InfoArea from './InfoArea';
+import ResultsArea from './ResultsArea';
 import '../styles/app.css';
-
 
 export default class App extends Component {
     render(){
         return (
             <div className="app">
                 <InfoArea />
+                <ResultsArea />
             </div>
         );
     }
