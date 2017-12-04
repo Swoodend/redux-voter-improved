40cba6480c69233a50996cd10655334b66eb8a2f
Scott Woodend <woodendscott@gmail.com>
2017-12-04 01:12:07 -0500

    unit test the <App /> component

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,15 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../src/components/App.js';
 import React from 'react';
 
 describe('<App />', () => {
+    let component = shallow(<App/>);    
     it('should render without crashing', () => {
-        let component = shallow(React.createElement(App));
         let inst = component.instance();
         expect(component.length).toEqual(1);
+    });
+
+    it('should render two child components', () => {
+        expect(component.children().length).toEqual(2);
     });
 });
