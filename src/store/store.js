08e4344ed8fb31ad41deba73237872bfa6660343
Scott Woodend <woodendscott@gmail.com>
2017-12-04 15:42:41 -0500

    do not need to use or keep past percentage data

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,12 @@
 import { votesReducer } from '../reducers/votesReducer';
 import { currentPercentageReducer } from '../reducers/currentPercentageReducer';
-import { pastPercentageReducer } from '../reducers/pastPercentageReducer';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger'
 
 const combinedReducers = combineReducers({
     "votes": votesReducer,
-    "currentPercentages": currentPercentageReducer,
-    "pastPercentages": pastPercentageReducer
+    "currentPercentages": currentPercentageReducer
 });
 
 const logger = createLogger();
